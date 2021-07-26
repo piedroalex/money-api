4e6f9d7908f1cfb073c4564817d8eb685f2912db
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-26 17:44:57 -0300

    Implementando o recurso para cadastrar uma nova categoria no banco de dados.

--- src/main/java/br/ne/pi/the/palm/money/api/resource/CategoriaResource.java
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/CategoriaResource.java
@@ -1,11 +1,18 @@
 package br.ne.pi.the.palm.money.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.ne.pi.the.palm.money.api.model.Categoria;
 import br.ne.pi.the.palm.money.api.repository.CategoriaRepository;
@@ -20,5 +27,13 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria retorno = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(retorno.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(retorno);
 	}
 }
