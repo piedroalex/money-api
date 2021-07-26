a8b1ce891460c7c6f3b370b63f4513ab783ea720
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-26 17:30:20 -0300

    Implementando o recurso para listar todas as categorias cadastradas no banco de dados.

---
+++ src/main/java/br/ne/pi/the/palm/money/api/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.ne.pi.the.palm.money.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ne.pi.the.palm.money.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
---
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package br.ne.pi.the.palm.money.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ne.pi.the.palm.money.api.model.Categoria;
+import br.ne.pi.the.palm.money.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar(){
+		return categoriaRepository.findAll();
+	}
+}
