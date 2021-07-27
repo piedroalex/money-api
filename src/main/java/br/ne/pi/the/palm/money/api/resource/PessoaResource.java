689f96bac4c853b084cec6eee60abda677fb41d7
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-26 23:12:03 -0300

    Implementando o recurso para remover pessoa.

--- src/main/java/br/ne/pi/the/palm/money/api/resource/PessoaResource.java
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/PessoaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ne.pi.the.palm.money.api.event.RecursoCriadoEvent;
@@ -47,5 +49,11 @@ public class PessoaResource {
 		Pessoa retorno = pessoaRepository.findOne(codigo);
 		return retorno != null ? ResponseEntity.ok(retorno) : ResponseEntity.notFound().build();
 		
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.delete(codigo);
 	}
 }
