c4e948c7b7e53a464e64a7c3b5d01d8d90b35f28
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-26 22:50:11 -0300

    Usando eventos para adicionar o header Location.

---
+++ src/main/java/br/ne/pi/the/palm/money/api/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,28 @@
+package br.ne.pi.the.palm.money.api.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.ne.pi.the.palm.money.api.event.RecursoCriadoEvent;
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent> {
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent event) {
+		HttpServletResponse response = event.getResponse();
+		Long codigo = event.getCodigo();
+		
+		this.adicionarHeaderLocation(response, codigo);
+	}
+	
+	private void adicionarHeaderLocation( HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(codigo).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+}
--- src/main/java/br/ne/pi/the/palm/money/api/resource/PessoaResource.java
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/PessoaResource.java
@@ -1,12 +1,13 @@
 package br.ne.pi.the.palm.money.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.ne.pi.the.palm.money.api.event.RecursoCriadoEvent;
 import br.ne.pi.the.palm.money.api.model.Pessoa;
 import br.ne.pi.the.palm.money.api.repository.PessoaRepository;
 
@@ -25,6 +26,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private PessoaRepository pessoaRepository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Pessoa> listar(){
@@ -34,9 +38,8 @@ public class PessoaResource {
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa retorno = pessoaRepository.save(pessoa);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(retorno.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		return ResponseEntity.created(uri).body(retorno);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, retorno.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(retorno);
 	}
 	
 	@GetMapping("/{codigo}")
