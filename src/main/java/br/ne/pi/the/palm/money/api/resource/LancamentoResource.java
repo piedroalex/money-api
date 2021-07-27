fe68dc12b7a7560d11ab6d81d585d7541daf0b55
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-27 12:41:29 -0300

    Implementando o recurso para remover um lançamento.

--- src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
@@ -12,12 +12,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ne.pi.the.palm.money.api.event.RecursoCriadoEvent;
@@ -83,6 +85,18 @@ public class LancamentoResource {
 		Lancamento retorno = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, retorno.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(retorno);
+	}
+	
+	/**
+	 * Remove um lançamento de acordo com um código.
+	 * 
+	 * @param Long codigo
+	 * @return ResponseEntity<Lancamento>
+	 */
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		lancamentoRepository.delete(codigo);
 	}
 	
 	@ExceptionHandler({PessoaInexistenteOuInativaException.class})
