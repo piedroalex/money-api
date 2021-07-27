4acefcf53bb02077c9593b762f76c0631b146d19
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-27 10:52:59 -0300

    Implementando o recursos para cadastrar um lançamento.

--- src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
@@ -2,18 +2,26 @@ package br.ne.pi.the.palm.money.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.ne.pi.the.palm.money.api.event.RecursoCriadoEvent;
 import br.ne.pi.the.palm.money.api.model.Lancamento;
 import br.ne.pi.the.palm.money.api.repository.LancamentoRepository;
 
 /** 
- * Classe de acesso ao recurso Pessoa.
+ * Classe de acesso ao recurso Lancamento.
  * 
  * @author Pedro Alex
  * */
@@ -24,6 +32,9 @@ public class LancamentoResource {
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	/**
 	 * Retorna uma lista com todas os lancamentos.
 	 * 
@@ -47,4 +58,16 @@ public class LancamentoResource {
 		
 	}
 	
+	/**
+	 * Salva um novo lançamento.
+	 * 
+	 * @param Lancamento lancamento, HttpServletResponse response
+	 * @return ResponseEntity<Lancamento>
+	 */
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento retorno = lancamentoRepository.save(lancamento);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, retorno.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(retorno);
+	}
 }
