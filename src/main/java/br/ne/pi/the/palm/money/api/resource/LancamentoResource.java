ffedbe760d8f5311ba93deccb7bf390e2bfb366c
Pedro Alex L Martins <piedroalex@gmail.com>
2021-07-28 15:03:38 -0300

    Adicionando permissões de acesso aos recursos de Pessoa e Lançamento.

--- src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -60,6 +61,7 @@ public class LancamentoResource {
 	 * @return Page<Lancamento>
 	 */
 	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable){
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
@@ -71,6 +73,7 @@ public class LancamentoResource {
 	 * @return ResponseEntity<Lancamento>
 	 */
 	@GetMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
 		Lancamento retorno = lancamentoRepository.findOne(codigo);
 		return retorno != null ? ResponseEntity.ok(retorno) : ResponseEntity.notFound().build();
@@ -84,6 +87,7 @@ public class LancamentoResource {
 	 * @return ResponseEntity<Lancamento>
 	 */
 	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 		Lancamento retorno = lancamentoService.salvar(lancamento);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, retorno.getCodigo()));
@@ -98,6 +102,7 @@ public class LancamentoResource {
 	 */
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and #oauth2.hasScope('write')")
 	public void remover(@PathVariable Long codigo) {
 		lancamentoRepository.delete(codigo);
 	}
