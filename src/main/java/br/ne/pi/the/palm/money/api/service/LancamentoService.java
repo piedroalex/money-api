1d8e9e81899f48ca7d783412cf92953610a3214f
Pedro Alex L Martins <piedroalex@gmail.com>
2021-09-13 23:35:10 -0300

    Gerando os bytes do relatório.

--- src/main/java/br/ne/pi/the/palm/money/api/service/LancamentoService.java
+++ src/main/java/br/ne/pi/the/palm/money/api/service/LancamentoService.java
@@ -1,14 +1,27 @@
 package br.ne.pi.the.palm.money.api.service;
 
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.ne.pi.the.palm.money.api.dto.LancamentoEstatisticaPessoa;
 import br.ne.pi.the.palm.money.api.model.Lancamento;
 import br.ne.pi.the.palm.money.api.model.Pessoa;
 import br.ne.pi.the.palm.money.api.repository.LancamentoRepository;
 import br.ne.pi.the.palm.money.api.repository.PessoaRepository;
 import br.ne.pi.the.palm.money.api.service.exception.PessoaInexistenteOuInativaException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class LancamentoService {
@@ -19,6 +32,23 @@ public class LancamentoService {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
+	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
+		List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
+		
+		Map<String, Object> parametros = new HashMap<>();
+		parametros.put("DT_INICIO", Date.valueOf(inicio));
+		parametros.put("DT_FIM", Date.valueOf(fim));
+		parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+		
+		InputStream inputStream = this.getClass().getResourceAsStream(
+				"/relatorios/lancamentos-por-pessoa.jasper");
+		
+		JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros,
+				new JRBeanCollectionDataSource(dados));
+		
+		return JasperExportManager.exportReportToPdf(jasperPrint);
+	}
+	
 	public Lancamento salvar(Lancamento lancamento) {
 		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
 		if(pessoa == null || pessoa.isInativo()) {
