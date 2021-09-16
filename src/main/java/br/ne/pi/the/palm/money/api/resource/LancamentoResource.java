14885cf77bd30a7f7127c64c893a951341a26412
Pedro Alex L Martins <piedroalex@gmail.com>
2021-09-16 19:36:50 -0300

    Recebendo arquivos na api.

--- src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
+++ src/main/java/br/ne/pi/the/palm/money/api/resource/LancamentoResource.java
@@ -1,5 +1,8 @@
 package br.ne.pi.the.palm.money.api.resource;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.ne.pi.the.palm.money.api.dto.LancamentoEstatisticaCategoria;
 import br.ne.pi.the.palm.money.api.dto.LancamentoEstatisticaDia;
@@ -62,6 +66,15 @@ public class LancamentoResource {
 	
 	@Autowired
 	private MessageSource messageSource;
+	
+	@PostMapping("/anexo")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
+	public String uploadAnexo(@RequestParam MultipartFile anexo) throws IOException {
+		OutputStream out = new FileOutputStream("C:\\Users\\piedr\\Desktop\\anexo--" + anexo.getOriginalFilename());
+		out.write(anexo.getBytes());
+		out.close();
+		return "ok";
+	}
 	
 	@GetMapping("/relatorios/por-pessoa")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
